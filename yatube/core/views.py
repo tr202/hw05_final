c2b3c1f0cb541d4dbc5c59de735a4ee8f29a1a13
tr202 <tr202@ya.ru>
2023-01-30 02:56:28 +0300

    Update custom errors pages

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -7,3 +7,10 @@ def csrf_failure(request, reason=''):
 
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
+
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
+
+
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
