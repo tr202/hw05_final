7193279a83df98c2324ff193fbb95258945adc36
tr202 <tr202@ya.ru>
2023-01-30 02:59:52 +0300

    Correct PEP8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -8,6 +8,7 @@ def csrf_failure(request, reason=''):
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
+
 def server_error(request):
     return render(request, 'core/500.html', status=500)
 
